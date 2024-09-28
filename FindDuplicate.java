638c1af74c24672bc774415f36506e8a9a1b0fb7
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-28 21:01:48 +0600

    find duplicate

---
+++ FindDuplicate.java
@@ -0,0 +1,15 @@
+public class FindDuplicate {
+    public static int findDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println(findDuplicate(new int[]{3,3,3,3}));
+    }
+}
